54cc918dafffa76cbfd90d10a1af013fbd37e85c
NAGMA KAPOOR <nagmakapoor@gmail.com>
2016-10-16 16:29:45 -0400

    Added new return value functions

--- StackDriver.c
+++ StackDriver.c
@@ -2,7 +2,12 @@
 #include <stdlib.h>
 #include "StackInterface.h"
 
+void pushReturnValueCheck(int returnValue, int value);
+void popReturnValueCheck(Stack *S, int returnValue);
+
 int main(void){
+	
+	/* Initialize empty stack */
 	Stack S;
 	InitializeStack(&S);
 
@@ -12,32 +17,33 @@ int main(void){
 
 	int returnValue, removedItem;
 	returnValue = pop(&S, &removedItem);
-
-	if (!isEmpty(&S) && returnValue){
-		printf("You've successfully removed the top item from the stack.\n");
-	}
-	else{
-		printf("Error. You tried removing an item from an empty stack.\n");
-	}
+	popReturnValueCheck(&S, returnValue);
 
 	for (int i = 7; i > 0; i--){
 		returnValue = push(i, &S);
-		if (returnValue){
-			printf("Success! You've added %i to the stack\n", i);
-		}
-		else{
-			printf("Error! You were unable to add %i to the stack.\n", i);
-		}
+		pushReturnValueCheck(returnValue, i);
 	}
 
 	for (int i = 7; i > 0; i--){
 		returnValue = pop(&S, &removedItem);
+		popReturnValueCheck(&S, returnValue);
+	}
+}
 
-		if(returnValue){
-			printf("Success! You've removed %i from the stack\n", removedItem);
-		}
-		else{
-			printf("Error! You were unable to remove the top item from the stack.\n");
-		}
+void pushReturnValueCheck(int returnValue, int value){
+	if (returnValue){
+		printf("Success! You've added %i to the stack\n", value);
+	}
+	else{
+		printf("Error! You were unable to add %i to the stack.\n", value);
+	}
+}
+
+void popReturnValueCheck(Stack *S, int returnValue){
+	if (!isEmpty(S) && returnValue){
+		printf("You've successfully removed the top item from the stack.\n");
+	}
+	else{
+		printf("Error. You tried removing an item from an empty stack.\n");
 	}
 }
